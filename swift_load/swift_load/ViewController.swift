e13d3ac1c0852f195fdd558dc4b1d59fe32c2fcf
KK K <wangmianfeng@wangmianfengdemacbook-pro.local>
2018-04-13 21:16:46 +0800

    gengxin

--- swift_load/swift_load/ViewController.swift
+++ swift_load/swift_load/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
         self.title = "疯子"
+        let label = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        self.view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
