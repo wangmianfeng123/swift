f7420b588718fe4929834920f64c5fbfbc4c6f6a
KK K <wangmianfeng@wangmianfengdemacbook-pro.local>
2018-04-13 21:12:45 +0800

    sa

---
+++ swift_load/swift_load/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  swift_load
+//
+//  Created by 王绵峰 on 2018/4/13.
+//  Copyright © 2018年 王绵峰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        self.title = "疯子"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
